96413c3a76a64192e823e304641a6ab413c280bb
russellokamura <russell.okamura@gmail.com>
2021-06-07 15:12:45 -0700

    added test functions for the client and server

--- client_modbus.py
+++ client_modbus.py
@@ -1,3 +1,27 @@
 from pyModbusTCP.client import ModbusClient
+from time import sleep
 
-client = ModbusClient(host)
+# Modbus Function Codes
+# 1 - Read Coil Status
+# 2 - Read Input Status
+# 3 - Read Holding Registers
+# 4 - Read Input Resgiters
+# 5 - Write Single Coil Status
+# 6 - Write Single Register
+# 15 - Multiple Coil Write
+# 16 - Multiple Register Write
+
+# Modbus Register Map Sets
+# 00001-09999 - Coils               (read/write)      (1-bit)
+# 10001-19999 - Discrete Inputs     (read only)       (1-bit)
+# 30001-39999 - Input Registers     (read only)       (16-bit)
+# 40001-49999 - Holding Registers   (read/write)      (16-bit)
+
+
+client = ModbusClient(host='192.168.0.159', port=12345)
+client.open()
+for i in range(5):
+   sleep(1)
+   print("Reading Holding Register 0: {}".format(client.read_holding_registers(0)))
+   client.write_single_register(1, i)
+   print("Reading Holding Register 0: {}".format(client.read_holding_registers(1)))
--- server_modbus.py
+++ server_modbus.py
@@ -1,3 +1,23 @@
-from pyModbusTCP.client import ModbusClient
+from pyModbusTCP.server import ModbusServer, DataBank
+from time import sleep
+from random import uniform
 
-client = ModbusClient(host)
+host = '192.168.0.159'
+port = 12345
+server = ModbusServer(host, port, no_block=True)
+
+try:
+   print("Starting server...")
+   server.start()
+   print("Server is online!")
+   state = [0]
+   while True:
+      DataBank.set_words(0, [int(uniform(0, 100))])
+      if state != DataBank.get_words(1):
+         state = DataBank.get_words(1)
+         print("Value of register 1 has changed to {}".format(state))
+         sleep(0.5)
+except KeyboardInterrupt:
+   print("Shutdown Server...")
+   server.stop()
+   print("Server is offline.")
